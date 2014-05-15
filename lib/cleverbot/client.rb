34831aeca8e69e756f4c7569da23dfd8f4c9a02c
Antoine Leroyer <leroyer.antoine.tone@gmail.com>
2014-05-15 23:59:36 +0200

    Fixing previous commit to pass tests

--- lib/cleverbot/client.rb
+++ lib/cleverbot/client.rb
@@ -38,7 +38,7 @@ module Cleverbot
     base_uri 'http://www.cleverbot.com'
 
     parser Parser
-    HEADERS = {
+    headers ({
       'Accept-Encoding' => 'gzip',
       'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64; rv:29.0) Gecko/20100101 Firefox/29.0',
       'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -49,7 +49,7 @@ module Cleverbot
       'Referer' => 'http://www.cleverbot.com',
       'Cache-Control' => 'no-cache, no-cache',
       'Pragma' => 'no-cache'
-    }
+    })
 
     # Holds the parameters for an instantiated Client.
     attr_reader :params
@@ -74,7 +74,7 @@ module Cleverbot
       body['stimulus'] = message
       body['icognocheck'] = digest HashConversions.to_params(body)
 
-      post(PATH, :body => body, :headers => HEADERS).parsed_response
+      post(PATH, :body => body).parsed_response
     end
 
     # Initializes a Client with given parameters.
