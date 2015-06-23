1c75f0e8072155ee38fe53884ff584e536f76185
sapslaj <saps.laj@gmail.com>
2015-06-23 09:38:37 -0400

    Added high level integration tests.

--- spec/cleverbot_spec.rb
+++ spec/cleverbot_spec.rb
@@ -1,4 +1,13 @@
 require File.expand_path(File.join(File.dirname(__FILE__), 'spec_helper'))
 
 describe Cleverbot do
+  describe "high level integration" do
+    context "with a client instance" do
+      let(:client) { Cleverbot::Client.new }
+
+      it 'should give a proper response' do
+        expect(client.write "hi.").to match(/\A[A-Z].+\z/)
+      end
+    end
+  end
 end
