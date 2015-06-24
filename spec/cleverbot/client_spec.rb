c0e406e1f7e57b2e38e7b55eba4d3ff1c6b59936
sapslaj <saps.laj@gmail.com>
2015-06-24 11:36:58 -0400

    Cleaning up include HTTParty test

--- spec/cleverbot/client_spec.rb
+++ spec/cleverbot/client_spec.rb
@@ -3,7 +3,9 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 describe Cleverbot::Client do
   subject { Cleverbot::Client }
 
-  it { should <= HTTParty }
+  it 'should include HTTParty' do |variable|
+    should <= HTTParty
+  end
 
   describe '::DEFAULT_PARAMS' do
     subject { Cleverbot::Client::DEFAULT_PARAMS }
